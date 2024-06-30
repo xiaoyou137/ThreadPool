b494c183dc255a1eb8136636a154703eaee357fb
xianghai <954509637@qq.com>
2024-06-29 23:49:10 -0700

    实现C++20新特性信号量Semaphore类

--- include/threadpool.hpp
+++ include/threadpool.hpp
@@ -12,19 +12,19 @@
 #include <iostream>
 
 using std::atomic_int;
+using std::bind;
 using std::condition_variable;
+using std::cout;
+using std::endl;
+using std::function;
+using std::make_unique;
 using std::mutex;
 using std::queue;
 using std::shared_ptr;
-using std::unique_ptr;
-using std::make_unique;
-using std::unique_lock;
 using std::thread;
+using std::unique_lock;
+using std::unique_ptr;
 using std::vector;
-using std::function;
-using std::bind;
-using std::cout;
-using std::endl;
 
 // 线程池模式枚举类
 enum class PoolMode
@@ -39,27 +39,27 @@ class Any
 public:
     Any() = default;
     ~Any() = default;
-    Any(const Any&) = delete;
-    Any& operator=(const Any&) = delete;
-    Any(Any&&) = default;
-    Any& operator=(Any&&) = default;
-
+    Any(const Any &) = delete;
+    Any &operator=(const Any &) = delete;
+    Any(Any &&) = default;
+    Any &operator=(Any &&) = default;
 
     // 一个可以接受任意类型数据的构造函数
-    template<typename T>
+    template <typename T>
     Any(T data) : base_(make_unique<Derived<T>>(data)) {}
 
     // 从Any类对象中提取原类型对象
-    template<typename T>
+    template <typename T>
     T Cast()
     {
-        Derived<T>* p = dynamic_cast<Derived<T>*>(base_.get());
-        if(p == nullptr)
+        Derived<T> *p = dynamic_cast<Derived<T> *>(base_.get());
+        if (p == nullptr)
         {
             throw "bad cast!";
         }
         return p->data_;
     }
+
 private:
     // 基类类型
     class Base
@@ -68,7 +68,7 @@ private:
         virtual ~Base() = default;
     };
     // 派生类类型（模板）
-    template<typename T>
+    template <typename T>
     class Derived : public Base
     {
     public:
@@ -77,6 +77,35 @@ private:
     };
 
     unique_ptr<Base> base_; // 基类类型的智能指针
+};
+
+// 实现一个semaphore类
+class Semaphore
+{
+public:
+    Semaphore(int limit = 0) : resLimit_(limit) {}
+    ~Semaphore() = default;
+
+    // 获取信号量
+    void wait()
+    {
+        unique_lock<mutex> lock(mtx_);
+        cond_.wait(lock, [&]()->bool { return resLimit_ > 0; });
+        resLimit_--;
+    }
+
+    // 增加信号量资源计数
+    void post()
+    {
+        unique_lock<mutex> lock(mtx_);
+        resLimit_++;
+        cond_.notify_all();
+    }
+    
+private:
+    int resLimit_;            // 资源计数
+    mutex mtx_;               // 互斥锁
+    condition_variable cond_; // 条件变量
 };
 
 // task抽象基类
@@ -96,8 +125,8 @@ public:
     Thread(ThreadFunc threadfunc);
     // 启动线程函数
     void start();
-private:
 
+private:
     ThreadFunc func_;
 };
 
@@ -121,18 +150,19 @@ public:
     void submitTask(shared_ptr<Task> sp);
 
     // 禁止拷贝构造和拷贝赋值
-    ThreadPool(const ThreadPool&) = delete;
-    ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool(const ThreadPool &) = delete;
+    ThreadPool &operator=(const ThreadPool &) = delete;
+
 private:
     // 线程入口函数
     void threadFunc();
 
 private:
-    vector<unique_ptr<Thread>> threads_;        // 线程对列
-    queue<shared_ptr<Task>> taskQue_; // 任务队列
-    int initThreadSize_;              // 初始线程数量
-    atomic_int taskCount_;            // 任务队列中任务的个数
-    int taskMaxThreshold_;            // 任务队列中任务最大数量
+    vector<unique_ptr<Thread>> threads_; // 线程对列
+    queue<shared_ptr<Task>> taskQue_;    // 任务队列
+    int initThreadSize_;                 // 初始线程数量
+    atomic_int taskCount_;               // 任务队列中任务的个数
+    int taskMaxThreshold_;               // 任务队列中任务最大数量
 
     condition_variable cvNotFull_;  // 任务队列未满，可以向其中提交任务
     condition_variable cvNotEmpty_; // 任务队列不空，工作线程可以从其中取出任务执行
