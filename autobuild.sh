55040d1a8350c2967b7601a569073aa4d9234c2c
xiaoyou137 <45346818+xiaoyou137@users.noreply.github.com>
2024-07-01 17:01:07 +0800

    Update autobuild.sh

---
+++ autobuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="build"
+DIR2="bin"
+DIR3="lib"
+
+if [ ! -d "$DIR" ]; then
+    mkdir -p "$DIR"
+fi 
+
+if [ ! -d "$DIR2" ]; then
+    mkdir -p "$DIR2"
+fi 
+
+if [ ! -d "$DIR2" ]; then
+    mkdir -p "$DIR2"
+fi 
+
+cd ./build && rm -f ./*
+cmake ..
+make
+cd ../lib
+sudo cp *.so /usr/local/lib
+ldconfig
